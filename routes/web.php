4567efe947f10155ffce5e2fbb3ce55da1419087
JHenrique <henryque.pc.game@gmail.com>
2025-03-31 23:07:46 -0300

    inicio da aula

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/','home');
+Route::get('/teste', function () {
+    $products = Product::all();
+    echo '<pre>';
+    print_r($products->toArray());
+});
+
